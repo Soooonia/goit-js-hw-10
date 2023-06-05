3bbbf7ce6c82018dea67b20aa411524ce54c0ee5
Soooonia <121182239+Soooonia@users.noreply.github.com>
2023-06-05 17:33:53 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,80 +1,4 @@
-// // import { fetchBreeds } from './cat-api.js';
 
-// // fetchBreeds().then(breeds => {
-  
-// //   console.log(breeds);
-// // });
-
-
-// import { fetchBreeds } from "./cat-api.js";
-
-
-// const breedSelect = document.querySelector(".breed-select");
-// const loader = document.querySelector(".loader");
-// const error = document.querySelector(".error");
-// const catInfo = document.querySelector(".cat-info");
-
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   loader.style.display = "block";
-//   error.style.display = "none";
-
-//   fetchBreeds()
-//     .then((breeds) => {
-//       breeds.forEach((breed) => {
-//         const option = document.createElement("option");
-//         option.value = breed.id;
-//         option.textContent = breed.name;
-//         breedSelect.appendChild(option);
-//       });
-//       loader.style.display = "none";
-//     })
-//     .catch(() => {
-//       error.style.display = "block";
-//       loader.style.display = "none";
-//     });
-// });
-// breedSelect.addEventListener("change", () => {
-//     const breedId = breedSelect.value;
-//     catInfo.innerHTML = "";
-//     catInfo.style.display = "none";
-//     loader.style.display = "block";
-  
-//     fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
-//       .then((response) => response.json())
-//       .then((data) => {
-//         const catImage = document.createElement("img");
-//         catImage.src = data[0].url;
-//         catInfo.appendChild(catImage);
-  
-//         return fetch(`https://api.thecatapi.com/v1/breeds/${breedId}`);
-//     })
-//     .then((response) => response.json())
-//     .then((data) => {
-//       const textContainer = document.createElement("div");
-//       textContainer.classList.add("text-container");
-//       const breedName = document.createElement("h2");
-//       breedName.textContent = data.name;
-//       textContainer.appendChild(breedName);
-
-//       const description = document.createElement("p");
-//       description.textContent = data.description;
-//       textContainer.appendChild(description);
-
-//       const temperament = document.createElement("p");
-//       temperament.innerHTML =`Temperament: <span class="temperament-word">${data.temperament}</span>`;
-//       textContainer.appendChild(temperament);
-//       catInfo.appendChild(textContainer);
-
-//       catInfo.style.display = "block";
-//       loader.style.display = "none";
-//     })
-//     .catch((error) => {
-//       console.error("Error fetching cat breed details:", error);
-//       error.style.display = "block";
-//       loader.style.display = "none";
-//     });
-// });
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 import Notiflix from 'notiflix';
 import SlimSelect from 'slim-select';
@@ -84,7 +8,7 @@ const select = document.querySelector('.breed-select');
 select.addEventListener('change', onChangeSelect);
 
 const divPicture = document.querySelector('.cat-info-picture');
-const divInfo = document.querySelector('.cat-info-desc');
+const divInfo = document.querySelector('.cat-info-d');
 const loader = document.querySelector('.loader');
 
 fetchAndRenderBreeds();
